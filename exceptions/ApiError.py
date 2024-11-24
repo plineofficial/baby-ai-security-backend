0ab5021a96f3174520a9b4d4b9e02418acd288d8
plineofficial <pliyne23@gmail.com>
2024-11-24 20:29:42 +0500

    Первый коммит

---
+++ exceptions/ApiError.py
@@ -0,0 +1,14 @@
+class ApiError(Exception):
+    def __init__(self, status, message, errors = {}):
+        self.status = status
+        self.message = message
+        self.errors = errors
+        
+        
+    @staticmethod
+    def bad_request(message = '', errors = {}):
+        return ApiError(400, message, errors)
+    
+    @staticmethod
+    def unauthorized_error():
+        return ApiError(401, 'Нет авторизации!')
---
+++ exceptions/exception_handler.py
@@ -0,0 +1,13 @@
+from exceptions.ApiError import ApiError
+
+def exception_handler(ex):
+    
+    if isinstance(ex, ApiError):
+        return {
+            'message': ex.message,
+            'errors': ex.errors
+        }, ex.status
+        
+    return {
+        'message': "Ошибка, попробуйте еще раз!"
+    }, 500
